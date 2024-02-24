00d134205e759eb518c3433efb3614339be8d55a
Dylan Sena <dcsena@umich.edu>
2024-02-24 14:40:11 -0800

    initial commit of JD upload with fireworks integ

--- data_upload/README.md
+++ data_upload/README.md
@@ -4,7 +4,20 @@ python -m venv venv/
 pip install -r requirements.txt
 ```
 
+# Environment Variables
+CHAT_ENDPOINT: Using Fireworks url https://api.fireworks.ai/inference/v1
+
+CHAT_API_KEY: Using Fireworks Api Key
+
+CHAT_MODEL: Chat Model to use
+
+VECTARA_API_KEY: Using Vectara Personal Api key
+
+VECTARA_CORPUS_ID: Vectara corpus for JDs
+
+VECTARA_CUSTOMER_ID: Vectara customerId
+
 # Run
 ```commandline
-VECTARA_API_KEY={redacted} VECTARA_CORPUS_ID={redacted} VECTARA_CUSTOMER_ID={redacted} python3 vectara_upload.py
+CHAT_ENDPOINT="https://api.fireworks.ai/inference/v1" CHAT_API_KEY={redacted} VECTARA_API_KEY={redacted} VECTARA_CORPUS_ID={redacted} VECTARA_CUSTOMER_ID={redacted} python3 vectara_upload.py
 ```
--- data_upload/vectara_upload.py
+++ data_upload/vectara_upload.py
@@ -1,5 +1,7 @@
 import os
 
+from bs4 import BeautifulSoup
+
 from data_upload.JobDescriptionTransformer import JobDescriptionTransformer
 from data_upload.VectaraClient import VectaraClient
 
@@ -8,14 +10,19 @@ VECTARA_CUSTOMER_ID = os.environ["VECTARA_CUSTOMER_ID"]
 VECTARA_CORPUS_ID = os.environ["VECTARA_CORPUS_ID"]
 VECTARA_API_KEY = os.environ["VECTARA_API_KEY"]
 
+CHAT_ENDPOINT = os.environ["CHAT_ENDPOINT"]
+CHAT_API_KEY = os.environ["CHAT_API_KEY"]
+CHAT_MODEL = os.environ["CHAT_MODEL"]
+
 def upload_jobs_to_vectara():
-    job_transformer = JobDescriptionTransformer()
+    job_transformer = JobDescriptionTransformer(CHAT_ENDPOINT, CHAT_API_KEY, CHAT_MODEL)
     vectara_client = VectaraClient(VECTARA_CUSTOMER_ID, VECTARA_CORPUS_ID, VECTARA_API_KEY)
     for file_name in os.listdir(INPUT_DIR):
         if ".html" not in file_name:
             continue
         with open(os.path.join(INPUT_DIR, file_name), 'r', encoding='utf-8') as file:
-            transformed_jd = job_transformer.transform(file)
+            text = BeautifulSoup(file.read()).get_text()
+            transformed_jd = job_transformer.transform(text)
             vectara_client.index_document(transformed_jd)
 
 
